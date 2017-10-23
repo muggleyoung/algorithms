98c8eec59191ee66353a2ddeec12048b99e2ac30
Yang Mei <muggleyoung@gmail.com>
2017-10-23 23:23:44 +0200

    add the stats and clean up generated file

---
+++ percolation/src/PercolationStats.java
@@ -0,0 +1,53 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    public double[] fractions;
+    public int trials;
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        this.trials = trials;
+
+        this.fractions = new double[trials + 1];
+        for (int i = 1; i <= trials; i++) {
+            Percolation percolation = new Percolation(n);
+            int row;
+            int col;
+            while (!percolation.percolates()) {
+                row = StdRandom.uniform(n);
+                col = StdRandom.uniform(n);
+                if (!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                }
+            }
+            int openSites = percolation.numberOfOpenSites();
+            this.fractions[i] = (double) openSites / (n * n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev() / Math.sqrt(trials));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev() / Math.sqrt(trials));
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+    }
+
+}
