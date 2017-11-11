03a36846ca03b90135a5ca4a758c09117fe5f3eb
Yang Mei <muggleyoung@gmail.com>
2017-11-11 21:51:13 +0100

    Dequeue

---
+++ queues/src/Deque.java
@@ -0,0 +1,141 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node previous;
+    }
+
+
+    // construct an empty deque
+    public Deque() {
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return size;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        if (head == null) {
+            head = new Node();
+            head.item = item;
+            head.previous = null;
+            head.next = null;
+            tail = head;
+        } else {
+            Node current = head;
+            head.item = item;
+            head.next = current;
+            head.previous = null;
+            current.previous = head;
+        }
+        size++;
+    }
+
+    // add the item to the end
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        if (tail == null) {
+            tail = new Node();
+            tail.item = item;
+            tail.previous = null;
+            tail.next = null;
+            head = tail;
+        } else {
+            Node current = tail;
+            tail.item = item;
+            tail.next = null;
+            tail.previous = current;
+            current.next = tail;
+        }
+        size++;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Node current = head;
+        if (head != tail) {
+            head = current.next;
+            head.previous = null;
+        } else {
+            head = null;
+            tail = null;
+        }
+        size--;
+        return current.item;
+    }
+
+    // remove and return the item from the end
+    public Item removeLast() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Node current = tail;
+        if (head != tail) {
+            tail = current.previous;
+            tail.next = null;
+        } else {
+            head = null;
+            tail = null;
+        }
+        size--;
+        return current.item;
+    }
+
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        private Node current = head;
+
+        public DequeIterator() {
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ queues/test/DequeTest.java
@@ -0,0 +1,138 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DequeTest {
+    @Test
+    void returnTrueWhenIsEmpty() {
+        Deque deque = new Deque();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void returnFalseWhenIsNotEmpty() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addFirst(firstItem);
+        assertFalse(deque.isEmpty());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionWhenAddFirstWithNull() {
+        Deque<Object> deque = new Deque<>();
+        assertThrows(IllegalArgumentException.class, () -> deque.addFirst(null));
+    }
+
+    @Test
+    void shouldAddOneItemFromHead() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addFirst(firstItem);
+        assertEquals(1, deque.size());
+    }
+
+    @Test
+    void shouldAddTwoItemsFromHead() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        int secondItem = 2;
+        deque.addFirst(firstItem);
+        deque.addFirst(secondItem);
+        assertEquals(2, deque.size());
+    }
+
+    @Test
+    void shouldRemoveFirstWhenThereIsOne() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addFirst(firstItem);
+        deque.removeFirst();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldRemoveFirstWhenThereIsMoreThanOne() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addFirst(firstItem);
+        deque.removeFirst();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldThrowNoSuchElementExceptionWhenRemoveFirstFromNull() {
+        Deque<Integer> deque = new Deque<>();
+        assertThrows(NoSuchElementException.class, deque::removeFirst);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionWhenAddLastWithNull() {
+        Deque<Object> deque = new Deque<>();
+        assertThrows(IllegalArgumentException.class, () -> deque.addLast(null));
+    }
+
+    @Test
+    void shouldAddOneItemFromTail() {
+        Deque<Integer> deque = new Deque<>();
+        int lastItem = 1;
+        deque.addLast(lastItem);
+        assertEquals(1, deque.size());
+    }
+
+    @Test
+    void shouldAddTwoItemsFromTail() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        int secondItem = 2;
+        deque.addLast(firstItem);
+        deque.addLast(secondItem);
+        assertEquals(2, deque.size());
+    }
+
+    @Test
+    void shouldRemoveLastWhenThereIsOne() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addLast(firstItem);
+        deque.removeLast();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldRemoveLastWhenAddedFirst() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addFirst(firstItem);
+        deque.removeLast();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldRemoveFistWhenAddedLast() {
+        Deque<Integer> deque = new Deque<>();
+        int firstItem = 1;
+        deque.addLast(firstItem);
+        deque.removeFirst();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldRemoveLastWhenThereIsMoreThanOne() {
+        Deque<Integer> deque = new Deque<>();
+        int item = 1;
+        deque.addLast(item);
+        deque.removeLast();
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    void shouldThrowNoSuchElementExceptionWhenRemoveLastFromNull() {
+        Deque<Integer> deque = new Deque<>();
+        assertThrows(NoSuchElementException.class, deque::removeLast);
+    }
+}
